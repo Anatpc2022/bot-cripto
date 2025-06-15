a28fd324f9b73ec1615c34dc3027289618471863
Anatpc2022 <ana.tpc2022@gmail.com>
2025-06-15 13:22:55 -0300

    Função loadWallet

--- backend/src/app-em.js
+++ backend/src/app-em.js
@@ -7,29 +7,72 @@ let WSS;
 function startTickerMonitor() {
   new Exchange().tickerStream(async (markets) => {
     const riberBot = RiberBot.getInstance();
-    let results = await Promise.all(markets.map((mkt) =>
-      riberBot.updateMemory(mkt.symbol, "TICKER", null, mkt)
-    ));
+    let results = await Promise.all(
+      markets.map((mkt) =>
+        riberBot.updateMemory(mkt.symbol, "TICKER", null, mkt)
+      )
+    );
     if (!results) return;
 
-    results = results.filter(r => r);
-        if (results.length)
-            results.map(r => WSS.broadcast({ notification: r }));//{ text, type: success|error }
+    results = results.filter((r) => r);
+    if (results.length) results.map((r) => WSS.broadcast({ notification: r })); //{ text, type: success|error }
   });
   logger("M-TICKER", "Ticker Monitor foi iniciado!");
 }
 
-function startUserDataMonitor(userId){
-    try{
-        //carregar saldos da carteira
+async function loadWallet(userId, executeAutomations = true) {
+  const exchange = new Exchange();
 
-        //configurar stream de user data
+  const info = await exchange.balance();
+  const riberBot = RiberBot.getInstance();
+  let results = await Promise.all(
+    Object.keys(info).map((item) =>
+      riberBot.updateMemory(
+        item,
+        `WALLET_${userId}`,
+        null,
+        info[item].available,
+        executeAutomations
+      )
+    )
+  );
 
-        logger("U-" + userId, "O Monitor de Dados do Usuário foi iniciado!");
-    }
-    catch(err){
-        logger("U-" + userId, "O Monitor de Dados do Usuário NÃO foi iniciado!\n" + (err.response ? JSON.stringify(err.response.data) : err.message));
-    }
+  const wallet = Object.keys(info).map((item) => {
+    return {
+      symbol: item,
+      available: info[item].available,
+      onOrder: info[item].onOrder,
+    };
+  });
+
+  if (results) {
+    results = results.filter((r) => r);
+    if (results.length) results.map((r) => WSS.broadcast({ notification: r })); //{ text, type: success|error }
+  }
+
+  return wallet;
+}
+
+function startUserDataMonitor(userId) {
+  try {
+    loadWallet(userId, false).catch((err) =>
+      logger(
+        "U-" + userId,
+        "A carteira NÃO foi carregada!\n" +
+          (err.body ? JSON.stringify(err.body) : err.message)
+      )
+    );
+
+    //configurar stream de user data
+
+    logger("U-" + userId, "O Monitor de Dados do Usuário foi iniciado!");
+  } catch (err) {
+    logger(
+      "U-" + userId,
+      "O Monitor de Dados do Usuário NÃO foi iniciado!\n" +
+        (err.body ? JSON.stringify(err.body) : err.message)
+    );
+  }
 }
 
 async function init(userId, wssInstance) {
