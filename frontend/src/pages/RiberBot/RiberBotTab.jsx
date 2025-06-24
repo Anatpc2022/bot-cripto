a711e9bf26fb1a05b6d111fadd10d70b4625527f
Anatpc2022 <ana.tpc2022@gmail.com>
2025-06-24 11:37:32 -0300

    React Select

--- frontend/src/pages/RiberBot/RiberBotTab.jsx
+++ frontend/src/pages/RiberBot/RiberBotTab.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 /**
  * props:
@@ -7,44 +7,74 @@ import { useState } from "react";
  * - hasSearch
  */
 function RiberBotTab(props) {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
 
-    const [search, setSearch] = useState("");
+  useEffect(() => {
+    if (!props.data) return;
 
-    function onSearchChange(evt) {
-        setSearch(evt.target.value.toUpperCase());
-    }
+    let arr = Object.entries(props.data).map((prop) => {
+      return {
+        key: prop[0],
+        value: prop[1],
+      };
+    });
 
-    return (
-        <>
-            {
-                props.hasSearch
-                    ? (
-                        <div className="row">
-                            <div className="col-3 my-3">
-                                <div className="form-group">
-                                    <label htmlFor="search">Procurar:</label>
-                                    <input type="text" id="search" className="form-control" value={search} onChange={onSearchChange} />
-                                </div>
-                            </div>
-                        </div>
-                    )
-                    : <></>
-            }
-            <div className="table-responsive divScrollRiberBot">
-                <table className="table table-flush table-sm table-hover">
-                    <thead>
-                        <tr>
-                            <th className="border-gray-200">Chave</th>
-                            <th className="border-gray-200">Valor</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {JSON.stringify(props.data)}
-                    </tbody>
-                </table>
+    if (search) arr = arr.filter((item) => item.key.indexOf(search) !== -1);
+
+    arr.sort((a, b) => {
+      if (a.key > b.key) return 1;
+      if (a.key < b.key) return -1;
+      return 0;
+    });
+
+    setData(arr);
+  }, [props.data, search]);
+
+  function onSearchChange(evt) {
+    setSearch(evt.target.value.toUpperCase());
+  }
+
+  return (
+    <>
+      {props.hasSearch ? (
+        <div className="row">
+          <div className="col-3 my-3">
+            <div className="form-group">
+              <label htmlFor="search">Procurar:</label>
+              <input
+                type="text"
+                id="search"
+                className="form-control"
+                value={search}
+                onChange={onSearchChange}
+              />
             </div>
-        </>
-    )
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+      <div className="table-responsive divScrollRiberBot">
+        <table className="table table-flush table-sm table-hover">
+          <thead>
+            <tr>
+              <th className="border-gray-200">Chave</th>
+              <th className="border-gray-200">Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((obj) => (
+              <tr key={props.id + obj.key}>
+                <td>{obj.key}</td>
+                <td>{JSON.stringify(obj.value)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
 export default RiberBotTab;
--- frontend/src/pages/RiberBot/RiberBotTest.jsx
+++ frontend/src/pages/RiberBot/RiberBotTest.jsx
@@ -1,15 +1,45 @@
+import { useState } from "react";
+
+import Select from "react-select";
+
 /**
  * props:
  * - data
  */
 function RiberBotTest(props) {
-    return (
-        <>
-            {
-                JSON.stringify(props.data)
-            }
-        </>
-    )
+  const [select, setSelect] = useState("");
+  const [index, setIndex] = useState("");
+  const [indexValue, setIndexValue] = useState({});
+
+  function onIndexChange(evt) {
+    setSelect({ label: evt.value, value: evt.value });
+    setIndex(evt.value);
+    setIndexValue(props.data[evt.value]);
+  }
+
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      width: 300,
+    }),
+  };
+  return (
+    <>
+      {" "}
+      <div className="row">
+        <div className="col-4">
+          <Select
+            value={select}
+            styles={customStyles}
+            onChange={onIndexChange}
+            options={Object.keys(props.data).map((item) => {
+              return { label: item, value: item };
+            })}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default RiberBotTest;
